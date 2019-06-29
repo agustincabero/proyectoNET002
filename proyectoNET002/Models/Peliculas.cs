9b87d95abdeee7c49325cf96e54cb4682d0c19ff
Agustín Cabero <agustincabero@gmail.com>
2019-06-29 16:59:49 -0300

    Agregar archivos de proyecto.

---
+++ proyectoNET002/Models/Peliculas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace proyectoNET002.Models
+{
+    public class Peliculas
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Director { get; set; }
+        public string Name { get; set; }
+    }
+}
